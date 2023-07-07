ea08816203897c3e4f4bcef67fc96422c496ed08
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-07-07 04:55:48 +0100

    Add 6-sum_mixed_list.py

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""6-sum_mixed_list module"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
+    """returns a float as sum"""
+    return sum(mxd_list)
