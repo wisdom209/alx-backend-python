a923a3bc58894615b1de6c7201e18fdb9e5777e1
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-07-07 04:44:59 +0100

    Added 5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""module defines complex types"""
+from typing import List, Tuple, Dict
+
+
+def sum_list(input_list: List[float]) -> float:
+    """adds a list of floats"""
+    return sum(input_list)
