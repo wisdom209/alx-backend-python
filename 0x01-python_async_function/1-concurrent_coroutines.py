dd6131fc9c2877ac7412d6f36ebd35182f9c6d18
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-07-10 14:30:03 +0100

    Python async tasks

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,7 +4,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """return a list of values in async"""
     seconds_list = []
     for _ in range(0, n):
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -4,7 +4,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
     """return a list of values in async"""
     seconds_list = []
     for _ in range(0, n):
