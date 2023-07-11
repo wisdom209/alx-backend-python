23cb16b5947ad59433220dc484aa3ed9e98a0551
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-07-11 13:22:37 +0100

    async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""async generator module"""
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator:
+    """generator coroutine"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
