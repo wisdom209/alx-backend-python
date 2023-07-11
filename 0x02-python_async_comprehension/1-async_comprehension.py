61095c8f41ffef84097c368009eeb19c488365ba
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-07-11 13:54:46 +0100

    async comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 """async comprehension module"""
-
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension
+async def async_comprehension() -> List[float]:
+    """async comprehension function"""
+    numbers = [x async for x in async_generator()]
+    return numbers
