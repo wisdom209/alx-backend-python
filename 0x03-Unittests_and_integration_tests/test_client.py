12c34cce5752bf0e61c2a92a8046ec4643ce33ec
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-07-31 15:13:32 +0100

    add test_public_repos

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -22,7 +22,16 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_method.assert_called_once()
 
     def test_public_repos_url(self):
-        """Test public repos"""
+        """Test public repos url"""
         with mock.patch('client.GithubOrgClient._public_repos_url') as mock_method:
             mock_method.return_value = 'github.com'
             self.assertEqual(GithubOrgClient._public_repos_url(), 'github.com')
+
+    def test_public_repos(self):
+        """Test public repos"""
+        with mock.patch('client.GithubOrgClient.public_repos') as mock_method:
+            mock_method.return_value = 'github.com'
+            result = GithubOrgClient.public_repos()
+            self.assertEqual(result, 'github.com')
+            mock_method.assert_called_once()
+            
