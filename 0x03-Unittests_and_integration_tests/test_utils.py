d27ed8c9dcd0df5cc325734d827e437a61ff9858
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-07-31 13:51:07 +0100

    test get json

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 "Test module for utils.py"
 import unittest
+from unittest import mock
 from parameterized import parameterized
 from utils import access_nested_map
+from utils import get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -15,7 +17,7 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, x, y, expected_result):
         """Test the function"""
-        self.assertEquals(access_nested_map(x, y), expected_result)
+        self.assertEqual(access_nested_map(x, y), expected_result)
 
     @parameterized.expand([
         ({}, ("a",), KeyError),
@@ -25,3 +27,19 @@ class TestAccessNestedMap(unittest.TestCase):
         """test the function exceptions"""
         with self.assertRaises(expected):
             access_nested_map(x, y)
+
+
+class TestGetJson(unittest.TestCase):
+    """Testing getJson class"""
+
+    @parameterized.expand([
+        ('http://example.com', {"payload": True}),
+        ('http://holberton.io', {"payload": False})
+    ])
+    def test_get_json(self, input, expected_result):
+        """test get_json function"""
+        with mock.patch('utils.requests.get') as Mock_class:
+            Mock_class.json.return_value = expected_result
+            result = get_json(input)
+            Mock_class.assert_called_once_with(input)
+            # self.assertEqual(result, expected_result)
